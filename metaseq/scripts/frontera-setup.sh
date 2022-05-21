873548245b051d158091440f53dbca25e028fe8b
Samuel Stevens <samuel.robert.stevens@gmail.com>
2022-05-21 14:41:05 -0500

    add CXX env variable

--- metaseq/scripts/frontera-setup.sh
+++ metaseq/scripts/frontera-setup.sh
@@ -4,6 +4,7 @@ module load gcc/9.1.0
 module load cuda/11.3 cudnn nccl
 module load python3/3.9.2
 export LD_LIBRARY_PATH=/usr/lib64:$LD_LIBRARY_PATH
+export CXX=g++
 
 echo
 echo "ACTIVATE YOUR VIRTUAL ENVIRONMENT NOW!"
